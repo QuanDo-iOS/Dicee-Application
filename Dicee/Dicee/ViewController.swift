7d085208cd44e2c183fbc1f4c12b31a825377799
QuanDoCoder <dracolucius05032002@gmail.com>
2021-11-09 16:57:55 +0700

    update

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -12,15 +12,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
+    var dice1Number = 1
+    //var dice2Number = 5
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5;
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][dice1Number]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][(6 - dice1Number) % 6]
+        dice1Number  = (dice1Number + 1) % 6
 
+    }
+    
 }
 
